a1b932a38a021ccec34f01ee6176a31279464c68
khairi-brahmi <khairibrahmi00@gmail.com>
2021-10-31 15:33:24 -0500

    docker config

--- TestService/TestService/settings.py
+++ TestService/TestService/settings.py
@@ -81,14 +81,15 @@ WSGI_APPLICATION = 'TestService.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'HOST': 'db', # set in docker-compose.yml
+        'PORT': 5432 # default postgres port
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
